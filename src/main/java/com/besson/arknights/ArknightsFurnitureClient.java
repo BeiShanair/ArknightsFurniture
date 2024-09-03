87e59fe298d1baf5295c562b7a66104354cc55e4
北山_Besson <137259636+BeiShanair@users.noreply.github.com>
2024-09-03 13:33:11 +0800

    Initial commit

---
+++ src/main/java/com/besson/arknights/ArknightsFurnitureClient.java
@@ -0,0 +1,10 @@
+package com.besson.arknights;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArknightsFurnitureClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
